2cda4aa1b8c62d201b4efa19d7edc552256e1168
khertlyn <khertlynl12@gmail.com>
2022-05-03 17:42:42 -0300

    novas anotacoes web

---
+++ projetos web/index.php
@@ -0,0 +1,14 @@
+<HTML>
+	<head>
+		<title> meu primeiro site em PHP!</title>
+	</head>
+	
+	<body>
+	
+		<?php
+			for ($i = 0 ; $i < 10 ; $i++ ) {
+				print ( "Linha número" . $i . "<br />");
+			}
+		?>
+	</body>
+</html>
