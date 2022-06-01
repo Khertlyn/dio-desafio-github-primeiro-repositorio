8b282f05ab19be308b970c9f34a742d21c2e8070
Khertlyn <khertlynl12@gmail.com>
2022-06-01 12:30:24 -0300

    inicio site anuncio e pre-anuncio

---
+++ anuncio-4bbm/src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from "../counter/counter";
+import SmartCounter from '../counter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/ifoodcounter'
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Ifoodcounter/>
+  </>
+  );
+}
+
+export default App;
