bcdec6fcd20114973d56d143526e596b4436e3d0
Khertlyn <khertlynl12@gmail.com>
2022-06-27 15:36:44 -0300

    Delete encurtador de URL directory

--- encurtador de URL/dist/controller/URLcontroler.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=URLcontroler.js.map
--- encurtador de URL/src/config/Constants.ts
+++
@@ -1,6 +0,0 @@
-
-export const config = {
-API_URL: "http://localhost:5000",
-MONGO_CONNECTION:
-    'mongodb+srv://khertlyn:<84113749>@url-encurtador-dio.tdsnnlm.mongodb.net/?retryWrites=true&w=majority'
-}
